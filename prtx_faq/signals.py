1d3546d084d6e74c633527281863b7fa99de34d2
Tobias Kunze <r@rixx.de>
2018-11-13 22:44:39 +0100

    Adding ourselves to the navbar is simple!

---
+++ prtx_faq/signals.py
@@ -0,0 +1,24 @@
+from django.dispatch import receiver
+from django.urls import resolve, reverse
+from django.utils.translation import ugettext_lazy as _
+
+from prtx_faq.prtx import PRTX
+
+if PRTX == 'pretalx':
+    from pretalx.orga.signals import nav_event
+elif PRTX == 'pretix':
+    from pretix.control.signals import nav_event
+
+
+@receiver(nav_event, dispatch_uid='faq_nav_entry')
+def navbar_info(sender, request, **kwargs):
+    url = resolve(request.path_info)
+    kwargs = {'event': request.event.slug}
+    if PRTX == 'pretix':
+        kwargs['organizer'] = request.organizer.slug
+    return [{
+        'label': _('FAQ'),
+        'icon': 'question-circle-o',
+        'url': reverse('plugins:prtx_faq:faq.list', kwargs=kwargs),
+        'active': 'faq' in url.url_name,
+    }]

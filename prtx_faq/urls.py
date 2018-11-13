9496b33fb6d5b13a2fca5869b73c67f5b5ec84ec
Tobias Kunze <r@rixx.de>
2018-11-13 22:57:25 +0100

    Add empty views and routing

---
+++ prtx_faq/urls.py
@@ -0,0 +1,43 @@
+from contextlib import suppress
+
+from django.conf.urls import url
+
+from . import views
+from prtx_faq.prtx import PRTX
+
+
+def get_urls():
+    urls = []
+    data = (
+        (r'faq/', views.FAQList.as_view(), 'faq.list'),
+        (r'faq/new/', views.FAQCreate.as_view(), 'faq.create'),
+        (r'faq/(?P<pk>[0-9]+)/', views.FAQEdit.as_view(), 'faq.edit'),
+        (r'faq/(?P<pk>[0-9]+)/delete/', views.FAQDelete.as_view(), 'faq.delete'),
+        (r'faq/(?P<pk>[0-9]+)/up/', views.faq_up, 'faq.up'),
+        (r'faq/(?P<pk>[0-9]+)/down/', views.faq_down, 'faq.down'),
+        (r'faq/category/', views.FAQCategoryList.as_view(), 'faq.category.list'),
+        (r'faq/category/new/', views.FAQCategoryCreate.as_view(), 'faq.category.create'),
+        (r'faq/category/(?P<pk>[0-9]+)/', views.FAQCategoryEdit.as_view(), 'faq.category.edit'),
+        (r'faq/category/(?P<pk>[0-9]+)/delete/', views.FAQCategoryDelete.as_view(), 'faq.category.delete'),
+        (r'faq/category/(?P<pk>[0-9]+)/up/', views.faq_category_up, 'faq.category.up'),
+        (r'faq/category/(?P<pk>[0-9]+)/down/', views.faq_category_down, 'faq.category.down'),
+    )
+    if PRTX == 'pretix':
+        base = r'^control/event/(?P<organizer>[^/]+)/(?P<event>[^/]+)/'
+        for entry in data:
+            urls.append(url(base + entry[0], entry[1], name=entry[2]))
+
+    elif PRTX == 'pretalx':
+        base = r'^orga/event/(?P<event>[^/]+)/'
+        for entry in data:
+            urls.append(url(base + entry[0], entry[1], name=entry[2]))
+        urls.append(
+            url(r'^(?P<event>[^/]+)faq/$', views.FAQView.as_view(), name='faq'),
+        )
+    return urls
+
+urlpatterns = get_urls()
+
+event_patterns = [
+    url(r'^faq/$', views.FAQView.as_view(), name='faq'),
+]
---
+++ prtx_faq/views.py
@@ -0,0 +1,63 @@
+from django.views.generic import ListView, DeleteView, FormView, UpdateView, TemplateView
+
+from prtx_faq.models import FAQ, FAQCategory
+from prtx_faq.forms import FAQCategoryForm, FAQForm
+from prtx_faq.prtx import PRTX
+
+
+class FAQView(TemplateView):  # TODO
+    template_name = 'prtx_faq/faq.{}.html'.format(PRTX)
+
+
+class FAQList(ListView):  # TODO
+    model = FAQ
+    template_name = 'prtx_faq/faq_list.{}.html'.format(PRTX)
+
+
+class FAQCreate(FormView):  # TODO
+    template_name = 'prtx_faq/faq_create.{}.html'.format(PRTX)
+
+
+class FAQEdit(UpdateView):  # TODO
+    model = FAQ
+    template_name = 'prtx_faq/faq_edit.{}.html'.format(PRTX)
+
+
+class FAQDelete(DeleteView):  # TODO
+    model = FAQ
+    template_name = 'prtx_faq/faq_delete.{}.html'.format(PRTX)
+
+
+def faq_up(request, *args, **kwargs):  # TODO
+    pass
+
+
+def faq_down(request, *args, **kwargs):  # TODO
+    pass
+
+
+class FAQCategoryList(ListView):  # TODO
+    model = FAQCategory
+    template_name = 'prtx_faq/faq_category_list.{}.html'.format(PRTX)
+
+
+class FAQCategoryCreate(FormView):  # TODO
+    template_name = 'prtx_faq/faq_category_create.{}.html'.format(PRTX)
+
+
+class FAQCategoryEdit(UpdateView):  # TODO
+    model = FAQCategory
+    template_name = 'prtx_faq/faq_category_edit.{}.html'.format(PRTX)
+
+
+class FAQCategoryDelete(DeleteView):  # TODO
+    model = FAQCategory
+    template_name = 'prtx_faq/faq_category_delete.{}.html'.format(PRTX)
+
+
+def faq_category_up(request, *args, **kwargs):  # TODO
+    pass
+
+
+def faq_category_down(request, *args, **kwargs):  # TODO
+    pass

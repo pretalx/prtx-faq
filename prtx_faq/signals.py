63e3e73d69453e4fe886e897e54f26696eeee9cb
Raphael Michel <mail@raphaelmichel.de>
2018-12-17 23:10:10 +0100

    Use nested navigation in pretix

--- prtx_faq/signals.py
+++ prtx_faq/signals.py
@@ -5,19 +5,49 @@ from prtx_faq.prtx import PRTX
 
 if PRTX == 'pretalx':
     from pretalx.orga.signals import nav_event
+
+
+    @receiver(nav_event, dispatch_uid='faq_nav_entry')
+    def navbar_info(sender, request, **kwargs):
+        url = resolve(request.path_info)
+        kwargs = {'event': request.event.slug}
+        if PRTX == 'pretix':
+            kwargs['organizer'] = request.organizer.slug
+        return [{
+            'label': _('FAQ'),
+            'icon': 'question-circle-o',
+            'url': reverse('plugins:prtx_faq:faq.list', kwargs=kwargs),
+            'active': 'faq' in url.url_name,
+        }]
+
 elif PRTX == 'pretix':
     from pretix.control.signals import nav_event
 
 
-@receiver(nav_event, dispatch_uid='faq_nav_entry')
-def navbar_info(sender, request, **kwargs):
-    url = resolve(request.path_info)
-    kwargs = {'event': request.event.slug}
-    if PRTX == 'pretix':
-        kwargs['organizer'] = request.organizer.slug
-    return [{
-        'label': _('FAQ'),
-        'icon': 'question-circle-o',
-        'url': reverse('plugins:prtx_faq:faq.list', kwargs=kwargs),
-        'active': 'faq' in url.url_name,
-    }]
+    @receiver(nav_event, dispatch_uid='faq_nav_entry')
+    def navbar_info(sender, request, **kwargs):
+        url = resolve(request.path_info)
+        kwargs = {'event': request.event.slug}
+        if PRTX == 'pretix':
+            kwargs['organizer'] = request.organizer.slug
+        return [
+            {
+                'label': _('FAQ'),
+                'icon': 'question-circle-o',
+                'url': reverse('plugins:prtx_faq:faq.list', kwargs=kwargs),
+                'active': False,
+                'children': [],
+            },
+            {
+                'label': _('Categories'),
+                'url': reverse('plugins:prtx_faq:faq.category.list', kwargs=kwargs),
+                'parent': reverse('plugins:prtx_faq:faq.list', kwargs=kwargs),
+                'active': 'faq.category' in url.url_name,
+            },
+            {
+                'label': _('Questions'),
+                'url': reverse('plugins:prtx_faq:faq.list', kwargs=kwargs),
+                'parent': reverse('plugins:prtx_faq:faq.list', kwargs=kwargs),
+                'active': 'faq' in url.url_name and 'category' not in url.url_name,
+            },
+        ]

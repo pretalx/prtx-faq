35e1bcd6cd21cd30a1fdda5c355fb61ddf55bc8d
Tobias Kunze <r@rixx.de>
2023-08-25 14:13:04 +0200

    Release v1.0.3

--- prtx_faq/apps.py
+++ prtx_faq/apps.py
@@ -11,14 +11,14 @@ class PluginApp(AppConfig):
         author = "Tobias Kunze"
         description = gettext_lazy("An FAQ plugin for pretalx and pretix")
         visible = True
-        version = "1.0.2"
+        version = "1.0.3"
 
     class PretalxPluginMeta:
         name = gettext_lazy("Frequently Asked Questions")
         author = "Tobias Kunze"
         description = gettext_lazy("An FAQ plugin for pretalx and pretix")
         visible = True
-        version = "1.0.2"
+        version = "1.0.3"
 
     def ready(self):
         from . import signals  # NOQA
